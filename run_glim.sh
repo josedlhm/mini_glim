06ca3e95bd9b1556a5fa4c52813ab3f286574b1f
josedlhm <jose.heros5@gmail.com>
2025-10-19 22:38:25 +0000

    added shit

--- run_glim.sh
+++ run_glim.sh
@@ -2,13 +2,15 @@
 # Usage:
 #   bash run_glim.sh /path/to/bag_dir [/path/to/glim_config]
 
-set -euo pipefail
+set -eo pipefail
 BAG_DIR="${1:?bag_dir (folder that contains the rosbag2 files)}"
 GLIM_CONFIG="${2:-/home/user/glim_config}"
 
 # ROS2 + Livox env
+
 source /opt/ros/humble/setup.sh
 source ~/ws_livox/install/setup.sh
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
 
 echo "[*] Running GLIM..."
 echo "    Bag:     $BAG_DIR"
